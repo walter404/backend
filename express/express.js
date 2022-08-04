8b182ec591fff05caea32c3b6e1f259ede203e7b
walter404 <walterame123@gmail.com>
2022-08-04 16:14:54 -0300

    Api fullrest

--- express/express.js
+++ express/express.js
@@ -1,29 +1,98 @@
-const express = require('express')
-const Contenedor = require('./contenedor')
-const contenedor = new Contenedor('./productos.txt')
-const app = express()
+const express = require('express');
+const app = express();
+const fs = require('fs');
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/public/index.html');
+});
 
-app.get('/productos', (req, res)=>{
-    contenedor.getAll().then((productos)=>{
-        res.send(productos)
+app.get('/api/products', (req,res)=>{
+    fs.readFile('./products.json', (err, data)=>{
+        if (err) throw err;
+        res.send(JSON.parse(data));
     });
 });
 
-app.get('/productoRandom', (req, res)=>{
-    contenedor.getAll().then((productos)=>{
-        let randomProd = Math.floor(Math.random() * productos.length);
-        let prod = productos[randomProd];
-        res.send(prod);
+app.get('/api/products/:id', (req, res)=>{
+    fs.readFile('./products.json', (err, data)=>{
+        if (err) throw err;
+        let products = JSON.parse(data);
+        let product = products.find((product)=> product.id === +req.params.id);
+        res.send(product);
+    });
+});
+
+app.post('/app/products', (req, res)=>{
+    fs.readFile('./products.json', (err, data)=>{
+        if (err) throw err;
+        let products = JSON.parse(data);
+        let newProduct = {...req.body, id: products.length + 1};
+        products.push(newProduct);
+        fs.writeFile(
+            './products.json',
+            JSON.stringify(products, null, 2),
+            (err)=>{
+                if(err) throw err;
+                res.send(newProduct)
+            }
+        );
+    });
+});
+
+app.put('/api/products/:id', (req, res)=>{
+    fs.readFile('./products.json', (err, data)=>{
+        if (err) throw err;
+        let products = JSON.parse(data);
+        let product = products.find((product) => product.id === +req.params.id);
+        if (product) {
+            product.nombre = req.body.nombre;
+            product.precio = req.body.precio;
+            product.thumbnail = req.body.thumbnail;
+            fs.writeFile(
+                './products.json',
+                JSON.stringify(products, null, 2),
+                (err) => {
+                    if (err) throw err;
+                    res.send(product)
+                }
+            );
+        } else {
+            res.send('producto no encontradi')
+        }
+    });
+});
+
+app.delete('/api/products/:id', (req, res)=>{
+    fs.readFile('./products.json', (err, data)=>{
+        if ( err) throw err ;
+        let products = JSON.parse(data);
+        let product = products.find((product) => product.id === +req.params.id);
+        if(product){
+            let newProducts = products.filter(
+                (product) => product.id !== +req.params.id
+            );
+            fs.writeFile(
+                './products.json',
+                JSON.stringify(newProducts, null, 2),
+                (err) => {
+                    if (err) throw err;
+                    res.send('Producto eleminado');
+                }
+            );
+        } else {
+            res.send('Producto no encontrado')
+        }
     });
 });
 
 
-const PORT = 8080
+const PORT = 8080;
 
 const server = app.listen(PORT, ()=>{
     console.log(`Escuchando en el puerto: ${server.address().port}`)
-})
+});
 
-server.on('error', err=> console.log(err))
+server.on('error', err=> console.log(err));
